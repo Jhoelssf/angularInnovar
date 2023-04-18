2ad204e88042cb0f2e0a64857088848745ed90dd
johanWill <>
2023-04-18 18:31:13 -0500

    formarray for abilities

--- src/app/pokemon-module/pokemons/pokemon-selected/pokemon-selected.component.ts
+++ src/app/pokemon-module/pokemons/pokemon-selected/pokemon-selected.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Pokemon } from '../model';
-import { FormArray, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-pokemon-selected',
@@ -8,18 +8,29 @@ import { FormArray, FormGroup } from '@angular/forms';
   styleUrls: ['./pokemon-selected.component.css']
 })
 export class PokemonSelectedComponent implements OnInit, OnDestroy {
-  @Input() pokemon: Pokemon | undefined = undefined;
-  formGroupAbilities: FormGroup = new FormGroup({
-    arrayMoves: new FormArray([])
+  @Input() pokemon!: Pokemon;
+  formGroupAbilities: FormGroup = this.fb.group({
+    abilities: this.fb.array([])
   })
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.pokemon.abilities.forEach(ability => {
+      this.abilities.push(this.fb.control(ability.ability.name));
+    });
   }
 
   ngOnDestroy(): void {
     console.log('pokemon se ha destruido')
+  }
+
+  get abilities(){
+    return this.formGroupAbilities.controls['abilities'] as FormArray;
+  }
+
+  addAbility(ability: string | null){
+    this.abilities.push(this.fb.control(ability));
   }
 
 }
