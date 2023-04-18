742242b3ed10668e009ce890d74b3e6a2bb22f06
johanWill <>
2023-04-18 12:23:10 -0500

    pokemons component created

--- src/app/pokemon-module/pokemon-module-routing.module.ts
+++ src/app/pokemon-module/pokemon-module-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { PokemonsComponent } from "./pokemons/pokemons.component";
 
 const routes: Routes = [
+  {path: '', component: PokemonsComponent}
 ]
 
 @NgModule({
--- src/app/pokemon-module/pokemon-module.module.ts
+++ src/app/pokemon-module/pokemon-module.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { PokemonModuleRoutingModule } from './pokemon-module-routing.module';
+import { PokemonsComponent } from './pokemons/pokemons.component';
 
 
 
 @NgModule({
   declarations: [
+    PokemonsComponent
   ],
   imports: [
     CommonModule,
