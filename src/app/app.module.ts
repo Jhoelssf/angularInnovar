6d961ce821777333914059347c7cffa5beda97ab
luis_jh <160544@unsaac.edu.pe>
2023-04-19 14:58:46 -0500

    creando navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,15 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigateBarComponent } from './navigate-bar/navigate-bar.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [AppComponent, HomeComponent, NavigateBarComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,17 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NavigateBarComponent } from './navigate-bar/navigate-bar.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
 const routes: Routes = [
+  /*
   {
     path: '',
     redirectTo: 'ejemplo',
     pathMatch: 'full',
   },
+  */
+  {
+    path: '',
+    component: HomeComponent,
+  },
   {
     path: 'home',
     component: HomeComponent,
   },
+  {
+    path: 'favorites',
+    component: FavoritesComponent,
+  },
+
   {
     path: 'ejemplo',
     loadChildren: () =>
