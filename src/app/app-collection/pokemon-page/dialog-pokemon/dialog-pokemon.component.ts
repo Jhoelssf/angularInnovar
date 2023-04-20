f914dc120e8d304de935799dbaac76cd29c386af
Melanie <171070@unsaac.edu.pe>
2023-04-20 07:15:58 -0500

    Add app-collection for pokemons

---
+++ src/app/app-collection/pokemon-page/dialog-pokemon/dialog-pokemon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatDialog , MatDialogRef} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog-pokemon',
+  templateUrl: './dialog-pokemon.component.html',
+  styleUrls: ['./dialog-pokemon.component.css']
+})
+export class DialogPokemonComponent {
+
+}
+
--- src/app/app-collection/pokemon-page/pokemon-page.component.ts
+++ src/app/app-collection/pokemon-page/pokemon-page.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { IPokemonData, IPokemonList, Pokemon } from '../model';
+import { DialogPokemonComponent } from './dialog-pokemon/dialog-pokemon.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-pokemon-page',
@@ -7,4 +11,32 @@ import { Component } from '@angular/core';
 })
 export class PokemonPageComponent {
 
+  pokemonList!: Pokemon[] | undefined;
+  objectPokemonList: IPokemonData[] | undefined = [];
+  baseUrl = 'https://pokeapi.co/api/v2';
+  constructor(private http: HttpClient, public dialog: MatDialog) {}
+
+  async ngOnInit() {
+    this.http.get<IPokemonList>(`${this.baseUrl}/pokemon?limit=20`).subscribe((response) => {
+      this.pokemonList = response.results;
+      for (let pokemon of response.results) {
+        this.onImagePokemon(pokemon.name);
+      }
+      console.log(this.pokemonList);
+    });
+  }
+
+
+  async onImagePokemon(idPokemon: number | string) {
+    this.http.get<IPokemonData>(`${this.baseUrl}/pokemon/${idPokemon}`).subscribe({
+      next: (response) => {
+        this.objectPokemonList?.push(response)
+        console.log(this.objectPokemonList);
+      }
+    });
+  }
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogPokemonComponent, {
+    });
+  }
 }
