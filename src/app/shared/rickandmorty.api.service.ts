c9d6fea666f6c95dc74e33c98f91bfe57d21bb4c
johanWill <>
2023-04-20 12:15:00 -0500

    rick and mority api service

---
+++ src/app/shared/rickandmorty.api.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, of, tap } from 'rxjs';
+import { RootCharacterObject } from '../rickandmorty/charactermodels';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RickandmortyApiService {
+  private rickandmortyUrl = 'https://rickandmortyapi.com/api';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getCharacters(page: number): Observable<RootCharacterObject> {
+    const url = `${this.rickandmortyUrl}/character/?page=${page}`;
+    return this.http.get<RootCharacterObject>(url)
+    .pipe(
+      tap(x => console.log('Fetched characters')),
+      catchError(this.handleError<RootCharacterObject>('getCharacters', undefined))
+    );
+  }
+
+  handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.log(error);
+      console.log(`${operation} failed: ${error.message}`);
+      return of(result as T)
+    } 
+  }
+}
