e07f89f7a0d5cb7f1101a2bfd90b77b4f11133ae
Jhoel Soncco <jhoel.soncco@provistacorp.com>
2023-04-20 08:37:03 -0500

    routing

--- src/app/pokemonapp/pokemon.routing.module.ts
+++ src/app/pokemonapp/pokemon.routing.module.ts
@@ -1,29 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pokemon/home/home.component';
 import { FavoritesComponent } from './pokemon/favorites/favorites.component';
+import { HomeComponent } from './pokemon/home/home.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
 
-
 const routes: Routes = [
-    {
+  {
+    path: '',
+    component: PokemonComponent,
+    children: [
+      {
         path: '',
-        component: PokemonComponent,
-        children: [
-            {
-                path: 'home',
-                component: HomeComponent
-            },
-            {
-                path: 'favorites',
-                component: FavoritesComponent
-            },
-        ]
-    },
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: 'favorites',
+        component: FavoritesComponent,
+      },
+    ],
+  },
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class PokemonRoutingModule { }
+export class PokemonRoutingModule {}
--- src/app/pokemonapp/pokemon/home/home.component.ts
+++ src/app/pokemonapp/pokemon/home/home.component.ts
@@ -1,61 +1,63 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Inject, OnInit } from '@angular/core';
-import { RootObject, Sprites } from './model';
-import { HttpClient } from "@angular/common/http"
-import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { Ability } from './model';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { RootObject } from './model';
 
 interface PokemonData {
-  pokemonData: RootObject
+  pokemonData: RootObject;
 }
-
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-
 export class HomeComponent implements OnInit {
-
   pokemon!: number;
   arrayPokemon: Array<RootObject> = [];
   baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
-  constructor(private http: HttpClient, private dialog: MatDialog) {
-
-  }
+  constructor(private http: HttpClient, private dialog: MatDialog) {}
   openDialog(Pokemon: RootObject): void {
-    console.log(Pokemon)
+    console.log(Pokemon);
     // this.pokemon = this.arrayPokemon.findIndex((element) => element.id == idPokemon)
     this.dialog.open(DialogPokemon, {
       width: '40rem',
       height: '35rem',
       data: {
-        pokemonData : Pokemon 
+        pokemonData: Pokemon,
         // namePokemon: this.arrayPokemon[this.pokemon].name,
         // abilitiesPokemon: this.arrayPokemon[this.pokemon].abilities,
         // heightPokemon: this.arrayPokemon[this.pokemon].height,
         // images: this.arrayPokemon[this.pokemon].sprites,
         // weightPokemon: this.arrayPokemon[this.pokemon].weight
-      }
+      },
     });
   }
 
   ngOnInit() {
     for (let i = 1; i < 20; i++) {
-      this.http.get<RootObject>(`https://pokeapi.co/api/v2/pokemon/${i}`).subscribe((res) => {
-        this.arrayPokemon.push(res)
-      })
+      this.http
+        .get<RootObject>(`https://pokeapi.co/api/v2/pokemon/${i}`)
+        .subscribe((res) => {
+          this.arrayPokemon.push(res);
+        });
     }
-    console.log(this.arrayPokemon)
+    console.log(this.arrayPokemon);
   }
 }
 
 @Component({
   selector: 'dialog-pokemon',
   templateUrl: 'dialog-pokemon.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class DialogPokemon {
-  constructor(public dialogRef: MatDialogRef<DialogPokemon>, @Inject(MAT_DIALOG_DATA) public data: PokemonData) { }
-
+  constructor(
+    public dialogRef: MatDialogRef<DialogPokemon>,
+    @Inject(MAT_DIALOG_DATA) public data: PokemonData
+  ) {}
 }
