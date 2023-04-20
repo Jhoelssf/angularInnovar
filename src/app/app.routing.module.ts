ea989be83b70c6f42d2676ae4874e71391faad57
johanWill <>
2023-04-20 12:24:49 -0500

    rickandmorty page default

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'ejemplo',
+    redirectTo: 'rickandmorty',
     pathMatch: 'full',
   },
   {
