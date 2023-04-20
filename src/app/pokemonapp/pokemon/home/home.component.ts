563be55b25dbe73a98c073a0788f34db0a5d00bd
mgvilc <miguelvilcac@gmail.com>
2023-04-20 00:30:47 -0500

    listPokemon, dialogPokemon, infoPokemon

--- src/app/pokemonapp/pokemon/home/home.component.ts
+++ src/app/pokemonapp/pokemon/home/home.component.ts
@@ -1,27 +1,62 @@
-import { Component, OnInit } from '@angular/core';
-import { RootObject } from './model';
+import { Component, Inject, OnInit } from '@angular/core';
+import { RootObject, Sprites } from './model';
 import { HttpClient } from "@angular/common/http"
+import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Ability } from './model';
+
+interface PokemonData {
+  namePokemon: string;
+  abilitiesPokemon: Array<Ability>;
+  heightPokemon: number;
+  weightPokemon: number;
+  images: Sprites
+}
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
-  arrayPokemon : Array<RootObject> = [];
+  pokemon!: number;
+  arrayPokemon: Array<RootObject> = [];
   baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient, private dialog: MatDialog) {
 
   }
-  ngOnInit() {
-    for(let i = 1; i<10; i++){
-      this.http.get<RootObject>(`https://pokeapi.co/api/v2/pokemon/${i}`).subscribe((res) => {
-      this.arrayPokemon.push(res)
-      console.log(this.arrayPokemon);
-      
-    })
-    }
-    
+  openDialog(idPokemon: number): void {
+    this.pokemon = this.arrayPokemon.findIndex((element) => element.id == idPokemon)
+    this.dialog.open(DialogPokemon, {
+      width: '40rem',
+      height: '35rem',
+      data: {
+        namePokemon: this.arrayPokemon[this.pokemon].name,
+        abilitiesPokemon: this.arrayPokemon[this.pokemon].abilities,
+        heightPokemon: this.arrayPokemon[this.pokemon].height,
+        images: this.arrayPokemon[this.pokemon].sprites,
+        weightPokemon: this.arrayPokemon[this.pokemon].weight
+      }
+    });
   }
+
+  ngOnInit() {
+    for (let i = 1; i < 20; i++) {
+      this.http.get<RootObject>(`https://pokeapi.co/api/v2/pokemon/${i}`).subscribe((res) => {
+        this.arrayPokemon.push(res)
+      })
+    }
+    console.log(this.arrayPokemon)
+  }
+}
+
+@Component({
+  selector: 'dialog-pokemon',
+  templateUrl: 'dialog-pokemon.html',
+  styleUrls: ['./home.component.css']
+})
+export class DialogPokemon {
+  constructor(public dialogRef: MatDialogRef<DialogPokemon>, @Inject(MAT_DIALOG_DATA) public data: PokemonData) { }
 
 }
--- src/app/pokemonapp/pokemonapp.module.ts
+++ src/app/pokemonapp/pokemonapp.module.ts
@@ -3,12 +3,15 @@ import { PokemonRoutingModule } from './pokemon.routing.module';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { PokemonComponent } from './pokemon/pokemon.component';
-import { HomeComponent } from './pokemon/home/home.component';
+import { DialogPokemon, HomeComponent } from './pokemon/home/home.component';
 import { FavoritesComponent } from './pokemon/favorites/favorites.component';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
-  declarations: [PokemonComponent, HomeComponent, FavoritesComponent],
-  imports: [ CommonModule, PokemonRoutingModule, HttpClientModule],
+  declarations: [PokemonComponent, HomeComponent, FavoritesComponent, DialogPokemon],
+  imports: [MatIconModule, MatButtonModule, MatDialogModule, MatCardModule, CommonModule, PokemonRoutingModule, HttpClientModule],
 })
 export class PokemonModule {}
