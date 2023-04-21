6d3cc2851425947f9d2a8222bc3f0059627229e9
unknown <angelplacido.E.H@gmail.com>
2023-04-20 22:52:18 -0500

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  title = 'primer-proyecto';
-  ngOnInit(): void {
-    const str1: string = 'ejemplo';
-    this.ejemplo(str1);
-  }
-
-  ejemplo(str: string = 'ejemplo2') {}
+export class AppComponent {
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
-import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
