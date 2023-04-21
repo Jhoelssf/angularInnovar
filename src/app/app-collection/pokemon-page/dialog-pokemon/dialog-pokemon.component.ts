1015b9ad6ef279405303fe05e9e4517b73f65fd1
Melanie <171070@unsaac.edu.pe>
2023-04-21 07:09:20 -0500

    Add dialog of pokemon

--- src/app/app-collection/pokemon-page/dialog-pokemon/dialog-pokemon.component.ts
+++ src/app/app-collection/pokemon-page/dialog-pokemon/dialog-pokemon.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { MatDialog , MatDialogRef} from '@angular/material/dialog';
+import { Component , Inject} from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog , MatDialogRef} from '@angular/material/dialog';
+import { IPokemonData } from 'src/app/shared/models/resGetPokemon.model';
+
 
 @Component({
   selector: 'app-dialog-pokemon',
@@ -7,6 +9,20 @@ import { MatDialog , MatDialogRef} from '@angular/material/dialog';
   styleUrls: ['./dialog-pokemon.component.css']
 })
 export class DialogPokemonComponent {
+  cardData: IPokemonData = {} as IPokemonData;
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogPokemonComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: IPokemonData,
+  ) {}
+
+  ngOnInit () :void {
+    this.cardData = this.data
+  }
+
+  addFavoritePokemon(pokemon: IPokemonData) {
+
+  }
 
 }
 
