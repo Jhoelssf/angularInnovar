67d75d066927800a6d117586fd02f7aa8ae10450
johanWill <>
2023-04-22 19:25:11 -0500

    auth service

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Character } from '../rickandmorty/charactermodels';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private username: string | undefined = undefined;
+  private favoritesSubject: BehaviorSubject<string | undefined> = new BehaviorSubject<string | undefined>(this.username);
 
   constructor() { }
 
+  getUsername(): Observable<string | undefined>{
+    return this.favoritesSubject.asObservable();
+  }
+
+  login(username: string): void{
+    if(username.indexOf(' ') < 0)
+      this.username = username;
+  }
+
+  isLoggedIn(): boolean{
+    return this.username !== undefined;
+  }
 
 }
