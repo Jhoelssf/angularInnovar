df3ff7ca41e49f6e75ecd6d06b9a16f6fb89d51b
unknown <angelplacido.E.H@gmail.com>
2023-04-23 02:44:40 -0500

    dialog 80%

--- src/app/kimetsu-no-yaiba/kimetsu-no-yaiba.module.ts
+++ src/app/kimetsu-no-yaiba/kimetsu-no-yaiba.module.ts
@@ -3,13 +3,12 @@ import { CommonModule } from '@angular/common';
 import { CardComponent } from './kimetsu-container/card/card.component';
 import { KimetsuContainerComponent } from './kimetsu-container/kimetsu-container.component';
 import { KimetsuNoYaibaRoutingModule } from './kimetsu-no-yaiba-routing.module';
-import { NavbarComponent } from './kimetsu-container/navbar/navbar.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { HttpClientModule } from '@angular/common/http';
 import { MatIconModule } from '@angular/material/icon';
 import {MatDialogModule} from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { DialogComponent } from './kimetsu-container/card/dialog/dialog.component';
+import {MatDividerModule} from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list'
 @NgModule({
   declarations: [KimetsuContainerComponent,CardComponent,DialogComponent],
   imports: [
@@ -18,6 +17,8 @@ import { DialogComponent } from './kimetsu-container/card/dialog/dialog.componen
     HttpClientModule,
     MatIconModule,
     MatDialogModule,
+    MatDividerModule,
+    MatListModule
 
   
   ]
