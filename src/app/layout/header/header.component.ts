de651dec7003e09f131267986b82f1e1d20ad31b
johanWill <>
2023-04-22 21:49:24 -0500

    show username

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { AuthService } from 'src/app/shared/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -9,14 +11,24 @@ export class HeaderComponent implements OnInit {
   menuList: {name: string, link: string}[] = [
     {name: 'Inicio', link: '/rickandmorty'}, 
     {name: 'Favoritos', link: '/rickandmorty/favorites'}, 
-    {name: 'Ejemplo', link: '/ejemplo'}];
-    
+    {name: 'Ejemplo', link: '/ejemplo'}
+  ];
+  username: string | undefined = undefined;
+  
+  unsubscribe$: Subject<boolean> = new Subject<boolean>();
+
   @Input() title: string | undefined = undefined;
   @Input() selected: undefined | 'Inicio' | 'Favoritos' | 'Ejemplo' = undefined;
   
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
+    this.auth
+      .getUsername()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((username)=>{
+        this.username = username; 
+      });
   }
 
 }
